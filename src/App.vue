9bdf0fa0fd5e892b48798db5540034ab191c3ea3
zj_1107 <1052893196@qq.com>
2020-08-16 15:46:19 +0800

    项目初始化-调整初始目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <!-- 路由占位符，通过路由规则匹配到的组件将会被渲染到这个位置，所以这个标签很重要，一定不能丢掉 -->
+    <router-view />
+    <h1>黑马头条</h1>
   </div>
 </template>
 
+<script>
+export default {
+  // 给根组件取个名字
+  name: 'App'
+}
+</script>
+
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载全局样式表
+import './styles/index.less'
+
 Vue.config.productionTip = false
 
 new Vue({
