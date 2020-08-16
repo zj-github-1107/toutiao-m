a116d072248903a9e2bd8fb7229021bc087e93ff
zj_1107 <1052893196@qq.com>
2020-08-16 23:38:49 +0800

    项目初始化-引入Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,18 @@
     <!-- 路由占位符，通过路由规则匹配到的组件将会被渲染到这个位置，所以这个标签很重要，一定不能丢掉 -->
     <router-view />
     <h1>黑马头条</h1>
+    <i class="iconfont icondianzan"></i>
+
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@ import store from './store'
 // 加载全局样式表
 import './styles/index.less'
 
+// 加载 Vant 组件库
+import Vant from 'vant'
+// 加载 Vant 的全局样式
+import 'vant/lib/index.css'
+// 注册使用 Vant 组件库
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
